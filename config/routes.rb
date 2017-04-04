b3e7c5bffb8d2a8a730dd0f1ef766450b9357153
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-04-04 12:46:46 +0200

    Configure routes for devise subjects teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'visitors#index'
+
   resources :students do
     get :subjects
   end
+
+  resources :teachers do
+    get :subjects
+  end
+
+  get 'reports/subjects', to: 'reports#subjects', as: :report_subjects
+
 end
