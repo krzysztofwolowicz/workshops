50f0895e9f5407a1ed839706e9e09177e2429088
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-04-05 10:14:10 +0200

    Add date of birth to students views, controller and helpers

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,5 +1,5 @@
 module StudentsHelper
   def student_params
-    params.require(:student).permit(:first_name, :last_name, subject_item_ids: [])
+    params.require(:student).permit(:first_name, :last_name, :birthdate, subject_item_ids: [])
   end
 end
