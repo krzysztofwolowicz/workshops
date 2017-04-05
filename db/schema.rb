48aa4d72b760590a2c70629b1061becdeae0539c
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-04-05 10:07:31 +0200

    Add birthdate column to students

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719115626) do
+ActiveRecord::Schema.define(version: 20170405080524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150719115626) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "birthdate"
   end
 
   create_table "subject_item_notes", force: :cascade do |t|
@@ -42,7 +43,6 @@ ActiveRecord::Schema.define(version: 20150719115626) do
   end
 
   add_index "subject_item_notes", ["student_id"], name: "index_subject_item_notes_on_student_id", using: :btree
-
 
   create_table "subject_items", force: :cascade do |t|
     t.string   "title"
@@ -79,8 +79,6 @@ ActiveRecord::Schema.define(version: 20150719115626) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-
   add_foreign_key "subject_item_notes", "students"
   add_foreign_key "subject_items", "teachers"
 end
-
