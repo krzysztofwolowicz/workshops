f91ab5e0d95d26e539468a6b815ee3845cb23ad3
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-04-05 13:04:42 +0200

    Add functionality to the controller and helper payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,30 @@
 class PaymentsController < ApplicationController
+  include PaymentsHelper
+  expose(:payments)
+  expose(:students)
+  expose(:payment, attributes: :payment_params)
+  before_action :authenticate_user!
+
+  def create
+    if payment.save
+      redirect_to payment_path(payment), notice: I18n.t('shared.created', resource: 'Payment')
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if payment.save
+      redirect_to payment_path(payment), notice: I18n.t('shared.updated', resource: 'Payment')
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if payment.destroy
+      redirect_to payment_path, notice: I18n.t('shared.deleted', resource: 'Payment')
+    end
+  end
+
 end
